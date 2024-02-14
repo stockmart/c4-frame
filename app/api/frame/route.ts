839c841d0fc885988b8a7ca38fad9ed60418a626
stockmart <mark.scott.mjs@gmail.com>
2024-02-14 12:34:07 -0600

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,7 +12,6 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <title>This is frame 11</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmSxiPikXDREXrcbaKSuNr4eaq4EQNxuFsojVsdd89w3eH/10.png" />
-    <meta property="fc:frame:image:aspect_ratio" content=1:1 />
     <meta property="fc:frame:button:1" content="Set Up an Open Audit" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:button:2" content="Check Out Code4rena Blue" />
@@ -23,6 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image:aspect_ratio" content="1:1" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmSxiPikXDREXrcbaKSuNr4eaq4EQNxuFsojVsdd89w3eH/${id}.png" />
     <meta property="fc:frame:button:1" content="Next Warden" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
